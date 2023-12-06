06bc24ba1e2e1721e96379204423fe1e99130be9
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-06 17:33:50 +0100

    Add NoProjectSelected component

---
+++ src/components/ComunicationComponents/NoProjectSelected.jsx
@@ -0,0 +1,10 @@
+export const NoProjectSelected = () => {
+    return(
+        <div>
+            <h2>No Project Selected</h2>
+            <p>Select a project or get started with a new one.</p>
+            <button>Create a new project.</button>
+        </div>
+        
+    )
+}
