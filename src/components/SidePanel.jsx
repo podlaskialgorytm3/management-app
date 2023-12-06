57279414995a121f4c0461362c4919dfa7be85fc
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-06 17:28:31 +0100

    Refactor SidePanel component styling

--- src/components/SidePanel.jsx
+++ src/components/SidePanel.jsx
@@ -1,9 +1,11 @@
 export const SidePanel = () => {
     return(
     <>
-        <div className="flex flex-col h-screen w-1/3 items-center justify-around pt-20 pb-20">
-            <h2>YOUR PROJECT</h2>
-            <button>+ Add project</button>
+        <div className="flex flex-col justify-around items-center w-1/3 h-screen px-8 py-16 bg-stone-900 text-stone-50 md:w-72 pt-20 pb-20">
+            <p>
+                <h2 className="text-xl font-bold text-whtie my-4">YOUR PROJECTS</h2>
+                <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">+ Add project</button>
+            </p>
             <p>
                 Examples projects ... 
             </p>
