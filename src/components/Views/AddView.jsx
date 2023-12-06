ce589481dc8001148a322a8af6dd562355faf1d1
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-06 18:35:33 +0100

    Refactor form content and separate form componets to others file

--- src/components/Views/AddView.jsx
+++ src/components/Views/AddView.jsx
@@ -1,13 +1,12 @@
+import { InputArea } from "../Molecules/InputArea"
+
 export const AddView = () => {
     return(
         <div>
-            <form>
-                <label>Title</label>
-                <input type="text"/>
-                <label>Description</label>
-                <input type="text"/>
-                <label>Due date</label>
-                <input type="date"/>
+            <form method="POST" className="mt-4 text-right flex flex-col items-center justify-around h-80 w-80">
+                <InputArea label="Title" type="text" />
+                <InputArea label="Description" type="text"/>
+                <InputArea label="Date due" type="date" />
             </form>
         </div>
     )
