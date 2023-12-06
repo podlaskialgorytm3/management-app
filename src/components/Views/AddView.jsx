474d3d9cf7e9ff3be4dd03b47854fdcb52b31837
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-06 18:04:23 +0100

    Add some custom elements to form, used for to adding projects

---
+++ src/components/Views/AddView.jsx
@@ -0,0 +1,14 @@
+export const AddView = () => {
+    return(
+        <div>
+            <form>
+                <label>Title</label>
+                <input type="text"/>
+                <label>Description</label>
+                <input type="text"/>
+                <label>Due date</label>
+                <input type="date"/>
+            </form>
+        </div>
+    )
+}
