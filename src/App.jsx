fe4da51b1bdce5cc3b0fbbdf3e1db40a4e9a61ea
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-06 18:04:40 +0100

    Add project view and no project selected view

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { SidePanel } from "./components/SidePanel";
 import { MainContent } from "./components/MainContent";
+import { useState } from 'react';
 
+const valueOfView = ["adding","project","no-selected"];
 
 function App() {
+  const [projects,setProjects] = useState()
+  const [view,setView] = useState("no-selected")
   return (
     <div className="flex flex-row">
       <SidePanel/>
-      <MainContent/>
+      <MainContent data={projects} view={view}/>
     </div>
   );
 }
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,7 +1,12 @@
-export const MainContent = () => {
+import { NoProjectSelected } from "./ComunicationComponents/NoProjectSelected"
+import { AddView } from "./Views/AddView"
+
+export const MainContent = ({view}) => {
+    console.log(view)
     return(
         <section className="flex flex-col w-2/3 h-screen items-center justify-center">
-            
+            {view == "adding" && <AddView />}
+            {view == "no-selected" && <NoProjectSelected />}
         </section>
     )
 }
