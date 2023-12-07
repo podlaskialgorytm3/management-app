08cf1dc2b024548dfe341f466d4d1c5117b2feb2
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-07 16:43:19 +0100

    Add title, custom text, and date components to ProjectView

---
+++ src/components/Atoms/Date.jsx
@@ -0,0 +1,3 @@
+export const Date = ({children}) => (
+    <p className="absolute top-0 right-0 text-stone-600 font-bold">{children}</p>
+) 
--- src/components/Views/ProjectView.jsx
+++ src/components/Views/ProjectView.jsx
@@ -1,7 +1,13 @@
+import { Title } from "../Atoms/Title"
+import { CustomText } from "../Atoms/CustomText"
+import { Date } from "../Atoms/Date"
+
 export const ProjectView = ({currentProject}) => {
     return(
-        <div>
-            
+        <div className="flex flex-col h-1/2 w-2/3 relative">
+            <Title>{currentProject.title}</Title>
+            <CustomText>{currentProject.description}</CustomText>
+            <Date>{currentProject.date}</Date>
         </div>
     )
 }
