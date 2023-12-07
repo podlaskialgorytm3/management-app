30629e9c9d0215598e7950ac2e23abc256f38d4e
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-07 16:43:54 +0100

    Add cursor-pointer to ProjectLink component

--- src/components/Atoms/ProjectLink.jsx
+++ src/components/Atoms/ProjectLink.jsx
@@ -1,7 +1,7 @@
 export const ProjectLink = ({children, ...props}) => {
     return(
         <p {...props}
-        className="h-10 w-full text-center text-xl leading-10 hover:bg-amber-950"
+        className="h-10 w-full text-center text-xl leading-10 hover:bg-amber-950 cursor-pointer"
         >{children}</p>
     )
 }
