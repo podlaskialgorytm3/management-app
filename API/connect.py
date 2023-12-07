1ebec867f9192894d927f80baf7fc93bd9abca34
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-07 23:22:15 +0100

    Refactor code with API

--- API/connect.py
+++ API/connect.py
@@ -1,35 +1,23 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 import mysql.connector
 
-# Utwórz połączenie z bazą danych MySQL
 db_connection = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
-    database="sklep"
+    database="management-db"
 )
 
-# Utwórz kursor do wykonywania zapytań SQL
 cursor = db_connection.cursor()
 
-# Inicjalizuj aplikację Flask
 app = Flask(__name__)
 
-# Definiuj endpoint do pobierania danych z bazy danych
 @app.route('/api/dane')
 def pobierz_dane():
-    # Przykładowe zapytanie SQL
-    zapytanie = "SELECT * FROM kategorie"
-
-    # Wykonaj zapytanie
-    cursor.execute(zapytanie)
-
-    # Pobierz wyniki
+    query = "SELECT * FROM projects"
+    cursor.execute(query)
     wyniki = cursor.fetchall()
-
-    # Zamień wyniki na format JSON i zwróć jako odpowiedź
     return jsonify(wyniki)
-
 
 if __name__ == '__main__':
     app.run(port=5000)
